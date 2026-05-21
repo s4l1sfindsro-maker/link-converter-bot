198343e9b8a004b15121fb9308f74208375ff108
S4l1sfinds_ro <s4l1sfindsro@gmail.com>
2026-05-21 20:23:54 +0300

    index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ client.on("messageCreate", async (message) => {
                         .setLabel("BBDBuy")
                         .setStyle(ButtonStyle.Link)
                         .setURL(result.bbdbuyUrl)
-                        .setEmoji("🛒")
+                        .setEmoji("🛒"),
 
                     new ButtonBuilder()
                         .setLabel("Original Link")
