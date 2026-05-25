72451b0ed59e03871f0c5e8bd38db15995953c84
S4l1sfinds_ro <s4l1sfindsro@gmail.com>
2026-05-25 09:40:05 +0300

    index.js

--- index.js
+++ index.js
@@ -59,11 +59,13 @@ client.on("messageCreate", async (message) => {
                         .setURL(result.originalUrl)
                         .setEmoji("🔗"),
 
-                    new ButtonBuilder()
-                        .setLabel("QC Finder")
-                        .setStyle(ButtonStyle.Link)
-                        .setURL(`https://finds.ly/product/${result.marketplace}/${result.itemId}`)
-                        .setEmoji("🔍")
+                   new ButtonBuilder()
+                  .setLabel("QC Finder")
+                  .setStyle(ButtonStyle.Link)
+                  .setURL(
+                    `https://www.uufinds.com/search?keyword=${encodeURIComponent(result.originalUrl)}`
+                  )
+                  .setEmoji("🔍")
                 );
 
                 await message.reply({
